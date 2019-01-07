8e24d368bece6d5661e233993bda8e818c9df72f
MarinaA2015 <m.abrashkina2015@gmail.com>
2019-01-07 16:12:06 +0200

    refactoring of LoginPageHelper and AccountCreatePageTests; new classes were created: AccountCreatePageHelper, MenuPageHelper, ProfilePageHelper

--- src/tests/AccountCreatePageTests.java
+++ src/tests/AccountCreatePageTests.java
@@ -1,50 +1,52 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.*;
 
 /**
  * Created by Inka on 22-Dec-18.
  */
 public class AccountCreatePageTests extends TestBase {
+    HomePageHelper homePage;
+    AccountCreatePageHelper accountCreatePage;
+    ProfilePageHelper profilePage;
+    MenuPageHelper menuPage;
+
+
+    @BeforeMethod
+    public void initPage() {
+        homePage = PageFactory
+                .initElements(driver, HomePageHelper.class);
+        accountCreatePage = PageFactory
+                .initElements(driver, AccountCreatePageHelper
+                        .class);
+        profilePage = PageFactory.initElements(driver,
+                ProfilePageHelper.class);
+        menuPage = PageFactory
+                .initElements(driver, MenuPageHelper.class);
+    }
+
     @Test
     public void createNewAccount(){
         String email1 = latinDigitString(8) + "@gmail.com";
-        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Create Account')]"),40);
-
-        WebElement createAccount = driver.findElement(By.xpath("//span[contains(text(),'Create Account')]"));
-        createAccount.click();
-
-        WebElement mailField = driver.findElement(By.xpath("//input[@formcontrolname='email']"));
-        mailField.click();
-        mailField.sendKeys(email1);
-
-
-        WebElement passwordField = driver.findElement(By.xpath("//input[@formcontrolname='password']"));
-        passwordField.click();
-        passwordField.sendKeys("example");
-
-        WebElement repPasswordField = driver.findElement(By.xpath("//input[@formcontrolname='passwordRep']"));
-        repPasswordField.click();
-        repPasswordField.sendKeys("example");
-
-        waitUntilElementIsLoaded(driver,By.xpath("//span[contains(text(),'Registration')]"),20);
-        WebElement registrationButton = driver.findElement(By.xpath("//span[contains(text(),'Registration')]"));
-        registrationButton.click();
-
-
-        waitUntilElementIsLoaded(driver,By.xpath("//button[@type='button']/span[contains(text(),'Cancel')]/.."),40);
-        waitUntilElementIsLoaded(driver,By.xpath("//mat-icon[@mattooltip='Menu']"),20 );
-
-        WebElement menuButton = driver.findElement(By.xpath("//mat-icon[@mattooltip='Menu']"));
-        menuButton.click();
-
-        waitUntilElementIsLoaded(driver, By.xpath("//span[@class='marginLeft']"), 20);
-        WebElement logOutMenu = driver.findElement(By.xpath("//span[@class='marginLeft']"));
-        logOutMenu.click();
+        homePage.waitUntilPageLoad();
+        homePage.pressCreateAccountButton();
+        accountCreatePage.waitUntilPageLoad();
+        accountCreatePage.enterValueToFieldEmail(email1);
+        accountCreatePage.enterValueToFieldPassword("example");
+        accountCreatePage.enterValueToFieldRepPassword("example");
+        accountCreatePage.pressRegistrationButton();
+        profilePage.waitUntilPageLoad();
+        profilePage.menuButtonClick();
+        menuPage.waitUntilPageLoad();
+        menuPage.pressLogOutButton();
+        homePage.waitUntilPageLoad();
+        Assert.assertEquals(homePage.getHeader(), "Shabbat in the family circle");
     }
 
     @Test
